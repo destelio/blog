61b991ee058804889a9d9577a7c9d6f87b8e1423
destelio <stelios.ioannides.risk@gmail.com>
2020-10-16 21:11:54 +0300

    adding IN urls.py

---
+++ urls.py
@@ -0,0 +1,51 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static 
+
+#from allauth.account.views import path
+from allauth.account.views import LoginView, SignupView 
+
+from django.contrib import admin
+from django.urls import path, include
+
+#from . import views
+
+from posts.views import (PostListView, PostDetailView, PostDeleteView,PostCreateView,PostUpdateView, like)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    #path('<account>/account_logout/', SignupView.as_view(), name="account_logout"),
+    #allauth.account.views.SignupView
+    path('', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('accounts/profile/', PostListView.as_view(), name='list'),
+
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('like/<slug>', like, name='like')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
